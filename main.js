2235d41ff00939da415854a0656be9019fcd28ea
yuanchongen <yuanchongen@58.com>
2021-07-19 20:33:20 +0800

    start

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
-import './style.css'
+import Vue from './src/index'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+var app = new Vue({
+    el: '#app',
+    // comments:true,
+    beforeCreate(){
+        console.log('%cbeforeCreate -------', 'color:red');
+        return 2222
+    },
+    created(){
+
+    },
+    beforeMount(){
+
+    },
+    mounted(){ //挂载元素，获取到DOM节点
+        console.log('%cmounted -------', 'color:red');
+       setTimeout(()=>{
+            // console.log('%cchange -------', 'color:red');
+            // console.log('%cthis : ', 'color:#53DCFB', this);
+           this.message = 'hello Msg change';
+//                console.log('this.count='+this.count)
+       }, 1000)
+    },
+    beforeUpdate(){
+
+    },
+    updated(){ //挂载元素，获取到DOM节点
+    },
+    beforeDestroy(){
+
+    },
+    destroyed(){
+
+   },
+    data:function () {
+         return  {
+             count:0,
+             message: 'Hello!'
+         }
+    },
+//        data: {
+//            count:0,
+//            message: 'Hello Vue!'
+//        }
+})
